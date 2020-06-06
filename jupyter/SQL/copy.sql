f4567bb6567c8eb77617641cd4177e14b3393935
Ilshat Karazbaev <i.karazbaev@mangotele.com>
2020-06-06 23:22:09 +0300

    slight fixes and copy commands

---
+++ jupyter/SQL/copy.sql
@@ -0,0 +1,5 @@
+\copy metrics_main (metric_id,category,subcategory,metric_name,extension,importance) FROM './postgresql_monitoring/jupyter/data/main/metrics_main.csv' DELIMITER ',' CSV HEADER;
+
+\copy metric_map_mamonsu (metric_id,metric_main_id) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/metric_map.csv' DELIMITER ',' CSV HEADER;
+\copy image_map_mamonsu (metric_id,image_id) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/image_map.csv' DELIMITER ',' CSV HEADER;
+\copy metrics_mamonsu (metric_id,metric_name,description) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/metrics.csv' DELIMITER ',' CSV HEADER;
---
+++ jupyter/SQL/mamonsu.sql
@@ -0,0 +1,40 @@
+CREATE TABLE metric_map_mamonsu (
+	metric_id                integer                 DEFAULT NULL,
+	metric_main_id           integer                 DEFAULT NULL
+);
+
+CREATE TABLE metrics_mamonsu (
+	metric_id                integer                                NOT NULL,
+	metric_name              text                    DEFAULT NULL           ,
+	category                 varchar(256)            DEFAULT NULL           ,
+	subcategory              varchar(256)            DEFAULT NULL           ,
+	extension                varchar(256)            DEFAULT NULL           ,
+	importance               varchar(256)            DEFAULT NULL           ,
+	description              text                    DEFAULT NULL           ,
+	PRIMARY KEY (metric_id)
+);
+
+CREATE TABLE image_map_mamonsu (
+	metric_id                integer                 DEFAULT NULL,
+	image_id                 integer                 DEFAULT NULL
+);
+
+CREATE TABLE dashboards_mamonsu (
+	dashboard_id             integer                 DEFAULT NULL,
+	dashboard_image_id       integer                 DEFAULT NULL,
+    dashboard_name           text                    DEFAULT NULL
+);
+
+CREATE TABLE images_mamonsu (
+	image_id                 integer                                 NOT NULL,
+	link                     text                                    NOT NULL,
+	PRIMARY KEY (image_id)
+);
+
+CREATE TABLE triggers_mamonsu (
+	metric_id                 integer                 DEFAULT NULL,
+	trigger_id                integer                 DEFAULT NULL,
+	expression                text                    DEFAULT NULL,
+	description               text                    DEFAULT NULL,
+	severity                  text                    DEFAULT NULL
+);
