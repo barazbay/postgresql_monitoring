94d208e803f195359032927d28e722e3fd5d3920
Ilshat Karazbaev <i.karazbaev@mangotele.com>
2020-06-06 23:52:58 +0300

    copy command

--- jupyter/SQL/copy.sql
+++ jupyter/SQL/copy.sql
@@ -3,3 +3,4 @@
 \copy metric_map_mamonsu (metric_id,metric_main_id) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/metric_map.csv' DELIMITER ',' CSV HEADER;
 \copy image_map_mamonsu (metric_id,image_id) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/image_map.csv' DELIMITER ',' CSV HEADER;
 \copy metrics_mamonsu (metric_id,metric_name,description) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/metrics.csv' DELIMITER ',' CSV HEADER;
+\copy images_mamonsu (image_id,link) FROM './postgresql_monitoring/jupyter/data/monitoring_tools/zabbix_mamonsu/images.csv' DELIMITER ',' CSV HEADER;
